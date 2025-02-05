3dc0514377cdea58454f05e5e1b5246ab89edb6b
Okpunor Emmanuel <okpunorrex@gmail.com>
2025-02-05 22:56:05 +0300

    fix

--- index.mjs
+++ index.mjs
@@ -16,7 +16,7 @@ app.use(cors())
 app.get("/api/classify-number", async (req, res) => {
   try {
     const number = parseInt(req.query.number)
-    if (!Number.isInteger(number) || number < 0) {
+    if (!Number.isInteger(number)) {
       return res.status(400).json({
         number: "alphabet",
         error: true,
@@ -33,7 +33,7 @@ app.get("/api/classify-number", async (req, res) => {
       is_perfect: isPerfectNumber(number),
       properties: checkNumberProperties(number),
       digit_sum: sumDigits(req?.query?.number),
-      fun_fact: result?.test,
+      fun_fact: result?.text,
     })
   } catch (error) {
     res.status(500).json({ error: true })
